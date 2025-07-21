2782a3f6388d2246dcbf80768b25cbe27277ce12
Christopher Guerrero <87549748+chr1stopherG@users.noreply.github.com>
2025-07-20 22:51:20 -0700

    updated js file to store items to cart

--- shoppingcart.js
+++ shoppingcart.js
@@ -1,24 +1,37 @@
  //Data structure holding items store has
  const products = {
-    dozenEggs: 7.49,
-    milkGallon: 5.99,
+    eggs: 7.49,
+    milk: 5.99,
     flour: 4.49,
     banana: .49, 
     butter: 3.99
-}//Looks through every item in this object(hopefully)
- console.log('test');
+}
+
 for(const item in products){
     const newButton = document.createElement('button');
     const buttonLabel = document.createTextNode(item);
     newButton.appendChild(buttonLabel)
-    const bodyElement = document.getElementById("store-items");
+    const bodyElement = document.getElementById('store-items');
     bodyElement.appendChild(newButton);
 }
 const buttonList = document.getElementsByTagName('button');
 
-
+const total = document.getElementById('cart-total');
+let cartId = 1;
+let totalPrice = 0;
+function addToCart(event) {
+    
+    const newItem = document.createElement('p')
+    const item = event.target.textContent;
+    newItem.innerHTML = cartId+'. ' + item;
+    const cart = document.getElementById('cart');
+    cart.appendChild(newItem);
+    cartId++;
+    totalPrice += products[item]
+    total.innerHTML = totalPrice.toFixed(2)
+}
 for(const btn of buttonList){
-    console.log('buttonList')
-    console.log(btn)
+    btn.addEventListener('click', (event)=> 
+        addToCart(event))
 
 }
